5226d7434da30f292bbd315c06ede710336c8e9e
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-01-13 18:54:38 +0300

    Update movie_recommendation.py

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -6,8 +6,9 @@ from sklearn.metrics.pairwise import linear_kernel
 
 @st.cache  
 def get_data_by_state():
-	return df1 = pd.read_csv('tmdb_5000_credits.csv')
-    return df2 = pd.read_csv('tmdb_5000_movies.csv')
+	df1 = pd.read_csv('tmdb_5000_credits.csv')
+	df2 = pd.read_csv('tmdb_5000_movies.csv')
+	return df1,df2 
 
 # Merge and preprocess the data as you did in your previous code
 df1.columns = ['id','tittle','cast','crew']
