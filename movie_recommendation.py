4590d475ba4a076431b76829a53c38c377770b58
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-01-13 19:32:59 +0300

    Update movie_recommendation.py

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -64,10 +64,9 @@ def get_recommendations(title,cosine_sim=cosine_sim):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:6]
     movie_indices = [i[0] for i in sim_scores]
-    last_df = df2[['title', 'release_dates','director','runtime', 'original_language', 'vote_average','genres']].iloc[movie_indices]
-    return last_df
+    return df2[['title', 'release_dates','director','runtime', 'original_language', 'vote_average','genres']].iloc[movie_indices]
 
-st.dataframe(last_df.style.hide_index())
+st.dataframe(df2[['title', 'release_dates','director','runtime', 'original_language', 'vote_average','genres']].iloc[movie_indices].style.hide_index())
 # Add CSS styles
 st.set_page_config(
     page_title="Movie Recommendation System",
