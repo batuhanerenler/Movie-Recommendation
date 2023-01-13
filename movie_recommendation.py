731131e3c0bc2194017b22820bc672f5c62a4eab
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-01-13 18:37:24 +0300

    Update movie_recommendation.py

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -78,7 +78,7 @@ st.markdown(
     <style>
         background { 
             rgb(2,0,36);
-            {
+        }
         background { 
             linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,9,1) 22%, rgba(0,212,255,1) 100%);
         }
